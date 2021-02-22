0f9481d491faa5710e4cdd12b9de2e6df20e32ad
donkere-vader <donkere.v@gmail.com>
2021-02-22 09:44:05 +0100

    Added print! and println! macros

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,19 @@
 #![no_std]
 
 use core::panic::PanicInfo;
+use core::fmt::Write;
 mod vga_buffer;
 
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    vga_buffer::println("Hello world!\nThis is CLOS\n\nuser@clos: ~$");
+    panic!("waaa");
 
     loop {};
 }
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {};
 }
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,3 +1,9 @@
+use core::fmt;
+use volatile::Volatile;
+use lazy_static::lazy_static;
+use spin::Mutex;
+use core::fmt::Write;
+
 #[allow(dead_code)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u8)]
@@ -20,33 +26,38 @@ pub enum Color {
     White = 15,
 }
 
-#[allow(dead_code)]
-pub fn print(s: &str) {
-    Writer::new(ColorCode::new(Color::White, Color::Black)).write_string(s);
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => ($crate::vga_buffer::_print(format_args!($($arg)*)));
 }
 
+#[macro_export]
+macro_rules! println {
+    () => ($crate::print!("\n"));
+    ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
+}
 
-#[allow(dead_code)]
-pub fn println(s: &str) {
-    let mut writer = Writer::new(ColorCode::new(Color::Black, Color::White));
-    writer.write_string(s);
-    writer.write_byte(b'\n');
+lazy_static! {
+    pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer::new(ColorCode::new(Color::Black, Color::White)));
 }
 
 pub struct Writer {
     column_position: usize,
-    row_position: usize,
     color_code: ColorCode,
     buffer: &'static mut Buffer,
+}
+
+#[doc(hidden)]
+pub fn _print(args:  fmt::Arguments) {
+    WRITER.lock().write_fmt(args).unwrap();
 }
 
 impl Writer {
     fn new(color_code: ColorCode) -> Writer {
         Writer {
             column_position: 0,
-            row_position: 0,
             color_code: color_code,
-            buffer: unsafe { &mut *(0xb8000 as *mut Buffer ) },
+            buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
         }
     }
 
@@ -54,18 +65,18 @@ impl Writer {
         match byte {
             b'\n' => self.new_line(),
             byte => {
-                if self.column_position >= BUFFFER_WIDTH {
+                if self.column_position >= BUFFER_WIDTH {
                     self.new_line();
                 }
 
-                let row = self.row_position;
+                let row = BUFFER_HEIGHT - 1;
                 let col = self.column_position;
 
                 let color_code = self.color_code;
-                self.buffer.chars[row][col] = ScreenChar {
+                self.buffer.chars[row][col].write(ScreenChar {
                     ascii_character: byte,
                     color_code: color_code,
-                };
+                });
                 self.column_position += 1;
             }
         }
@@ -80,12 +91,33 @@ impl Writer {
         }
     }
 
-    fn new_line(&mut self) {
-        self.row_position += 1;
-        self.column_position = 0;
-        if self.row_position >= BUFFER_HEIGHT {
-            self.row_position = 0;
+    fn clear_row(&mut self, row: usize) {
+        let blank = ScreenChar {
+            ascii_character: b' ',
+            color_code: self.color_code,
+        };
+        for col in 0..BUFFER_WIDTH {
+            self.buffer.chars[row][col].write(blank);
         }
+    }
+
+    fn new_line(&mut self) {
+        self.clear_row(0);
+        for row in 1..BUFFER_HEIGHT {
+            for col in 0..BUFFER_WIDTH {
+                let character = self.buffer.chars[row][col].read();
+                self.buffer.chars[row - 1][col].write(character);
+            }
+        }
+        self.clear_row(BUFFER_HEIGHT - 1);
+        self.column_position = 0;
+    }
+}
+
+impl fmt::Write for Writer {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.write_string(s);
+        Ok(())
     }
 }
 
@@ -96,12 +128,12 @@ struct ScreenChar {
     color_code: ColorCode,
 }
 
-const BUFFER_HEIGHT: usize= 25;
-const BUFFFER_WIDTH: usize = 80;
+const BUFFER_HEIGHT: usize = 25;
+const BUFFER_WIDTH: usize = 80;
 
 #[repr(transparent)]
 struct Buffer {
-    chars: [[ScreenChar; BUFFFER_WIDTH]; BUFFER_HEIGHT],
+    chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT],
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -110,7 +142,6 @@ struct ColorCode(u8);
 
 impl ColorCode {
     fn new(foreground_color: Color, background_color: Color) -> ColorCode {
-        ColorCode((foreground_color as u8 ) << 4 | background_color as u8)
+        ColorCode((foreground_color as u8) << 4 | background_color as u8)
     }
 }
-
